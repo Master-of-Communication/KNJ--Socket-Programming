3b7b35ba3817a0169f9b5ba914663314d9d01c50
Suman Kafle <suman.kafle@haw-hamburg.de>
2024-06-12 20:11:42 +0200

    skeleton added

--- assigments/http_server/http_server.py
+++ assigments/http_server/http_server.py
@@ -1,29 +1,41 @@
+# Author: Suman Kafle
+# Description: This code is a basic HTTP server that listens for incoming client requests,
+                #retrieves requested files, and sends them back to the client.
 #import socket module
 from socket import *
-#HOST = '127.0.0.1'
-#print ("\n" + gethostbyname(gethostname()))
-HOST = gethostbyname(gethostname())
+
 server_socket = socket(AF_INET, SOCK_STREAM)
-#Prepare a server socket
-server_port = 7001
-server_socket.bind((HOST, server_port)) #associate the server port number with this socket
-server_socket.listen(1)  #wait and listen for some client to knock on the door
+
+'''
+Prepare a server socket, e.g address and port then bind the server socket to the address and port
+dont forgent to listen for incoming connections
+'''
+HOST = '127.0.0.1'
+# Your code comes here
+# port number
+#associate the server port number with this socket
+#wait and listen for some client to knock on the door, the number of clients that can wait is N>0
+# Your code ends here
+
 while True:
     #Establish the connection
-    print('Ready to serve...')
-    connection_socket, addr = server_socket.accept() #accept the connection
-    print("Request accepted from (address, port) tuple: %s" % (addr,))
+    print('Server is running......')
+    #accept the connection
+    connection_socket, addr =  # Your code comes here 
+    print("Request accepted from (address, port) tuple: %s \n" % (addr,))
+
     try:
         message = connection_socket.recv(1024) #receive the message from the client
-        print('-------the message is------- ', message)
+        print('the message is : \n', message)
         filename = message.split()[1]
-        print('-------the filename is------- ', filename)
+        print('the filename is: ', filename)
         f = open(filename[1:])
         outputdata = f.read() #read the file
         #Send one HTTP header line into socket
         connection_socket.send('HTTP/1.1 200 OK\n\n'.encode())
         #Send the content of the requested file to the client
-        print('-------the length is------- ', len(outputdata))
+        print('the length is \n\n', len(outputdata))
+
         for i in range(0, len(outputdata)):
             connection_socket.send(outputdata[i].encode())
         connection_socket.send("\r\n".encode())
@@ -32,7 +44,8 @@ while True:
         #Send response message for file not found
         connection_socket.send('404 Not Found'.encode())
         #Close client socket
-        connection_socket.close()
-
-    server_socket.close()
+        # Your code comes here
+        
+    #Close server  socket
+    # Your code comes here
 ##
